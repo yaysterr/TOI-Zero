d3cdce1882efb8012f85e53e192964a50308996c
yaysterr <vivoyou3632@gmail.com>
2026-03-21 20:16:25 +0700

    21 3 26 🪂

--- A1/011.cpp
+++ A1/011.cpp
@@ -1,6 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-int main(){
-    
+int main() {
+    string s; cin >> s;
+    int count = 1;
+    for (int i = 1; i < s.length(); i++) {
+        if (s[i] == s[i - 1]) {
+            count++;
+        } else {
+            cout << count << s[i - 1];
+            count = 1;
+        }
+    }
+    cout << count << s[s.length() - 1];
+    return 0;
 }
